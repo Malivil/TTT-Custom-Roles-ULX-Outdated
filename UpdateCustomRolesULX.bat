43a901585427c41383bfc9741adeb2edfb50259d
Malivil <mlebson@outlook.com>
2020-10-15 15:32:21 -0400

    Moved distribution scripts to their own repo

--- UpdateCustomRolesULX.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p changes="Enter Changes: "
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"  update -addon "TTT-Custom-Roles-ULX.gma" -id "2091700901" -changes "%changes%"
-pause
