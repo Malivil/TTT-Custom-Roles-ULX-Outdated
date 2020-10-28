d35d8298dbf2cb2d359ddce78fb89dd3003659ae
Malivil <mlebson@outlook.com>
2020-10-28 11:50:24 -0400

    Added support for ttt_swapper_respawn_health

--- lua/ulx/xgui/gamemodes/terrortown.lua
+++ lua/ulx/xgui/gamemodes/terrortown.lua
@@ -300,13 +300,13 @@ local function AddGameplayModule()
 
     -- Role Configs
     local gptrcfgclp = vgui.Create("DCollapsibleCategory", gppnl)
-    gptrcfgclp:SetSize(390, 740)
+    gptrcfgclp:SetSize(390, 760)
     gptrcfgclp:SetExpanded(0)
     gptrcfgclp:SetLabel("Role Configs")
 
     local gptrcfglst = vgui.Create("DPanelList", gptrcfgclp)
     gptrcfglst:SetPos(5, 25)
-    gptrcfglst:SetSize(390, 740)
+    gptrcfglst:SetSize(390, 760)
     gptrcfglst:SetSpacing(5)
 
     local dsearch = xlib.makecheckbox { label = "ttt_detective_search_only (def. 1)", repconvar = "rep_ttt_detective_search_only", parent = gptrcfglst }
@@ -404,6 +404,9 @@ local function AddGameplayModule()
 
     local jwbt = xlib.makecheckbox { label = "ttt_jester_win_by_traitors (def. 1)", repconvar = "rep_ttt_jester_win_by_traitors", parent = gptrcfglst }
     gptrcfglst:AddItem(jwbt)
+
+    local srh = xlib.makeslider { label = "ttt_swapper_respawn_health (def. 100)", min = 25, max = 150, repconvar = "rep_ttt_swapper_respawn_health", parent = gptrcfglst }
+    gptrcfglst:AddItem(srh)
 
     local mshop = xlib.makeslider { label = "ttt_shop_merc_mode (def. 0)", min = 0, max = 4, repconvar = "rep_ttt_shop_merc_mode", parent = gptrcfglst }
     gptrcfglst:AddItem(mshop)
